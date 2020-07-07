c6a63628195561b37527ea085b32ec88a943b75a
marco <marcosigurta.ovb@gmail.com>
2020-07-07 11:26:07 +0200

    Created sub-menu links. Jquery and @media fixed to show or hide content on devices changes or onClick

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-$(document).ready(function(){
-    $(".nav-dropdown").mouseenter(function() {
+$(document).ready(function () {
+    $(".nav-dropdown").mouseenter(function () {
         $(this).children(".nav-dropdownMenu").show();
     });
 
-    $(".nav-dropdown").mouseleave(function() {
+    $(".nav-dropdown").mouseleave(function () {
         $(this).children(".nav-dropdownMenu").hide();
     });
 
-    $(".hidden-nav .fa-bars").click(function(){
+    $(".hidden-nav .fa-bars").click(function () {
         $(".hidden-content").show(1000);
         $(".hidden-nav").hide(1000);
         $("#main").hide();
     });
 
-    $(".hidden-nav-content .fa-times").click(function(){
+    $(".hidden-nav-content .fa-times").click(function () {
         $(".hidden-nav").show(1000);
         $(".hidden-content").hide(1000);
         $("#main").show();
     });
 
-    $(".dropdown-links").click(function(){
-        $(this).children(".ciao")();
-    })
+    $(".submenu-links-container").click(function () {
+        $(this).children(".submenu-ul").show(500);
+        $(this).find(".hidden-menu-link .fas").addClass('fa-chevron-up');
+    });
+
+    $(".submenu-links-container").mouseleave(function () {
+        $(this).children(".submenu-ul").hide(500);
+        $(this).find(".hidden-menu-link .fas").removeClass('fa-chevron-up');
+    });
+
 });
 
